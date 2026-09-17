f364a467b24f785dd75a1bd43d74f76bbbcde1bf
Samana Dahal <samanadahal11@gmail.com>
2026-09-17 17:07:07 +0545

    completed

--- src/stringsMethods/questionFive.java
+++ src/stringsMethods/questionFive.java
@@ -1,16 +1,22 @@
-//package stringsMethods;
-//
-//public class questionFive {
-//    public static void main(String[] args){
-//
-//
-//    }
-//    public String hello(String name){
-//        int count =0;
-//        for(int i = 0; i<name.length();i ++){
-//
-//
-//
-//        }
-//    }
-//}
+package stringsMethods;
+
+import java.util.Scanner;
+
+public class questionFive {
+    public static void main(String[] args){
+        System.out.println(five());
+    }
+    public static int five(){
+        Scanner src = new Scanner(System.in);
+        System.out.print("enter a word: ");
+        String a = src.nextLine();
+        int count =0;
+        String b= a.toLowerCase();
+        for(int i=0;i<a.length();i++) {
+            if(b.charAt(i)=='a'||b.charAt(i)=='e' ||b.charAt(i)=='i' ||b.charAt(i)=='o' ||b.charAt(i)=='u'){
+                count++;
+            }
+            }
+        return count;
+    }
+}
--- src/stringsMethods/questionFour.java
+++ src/stringsMethods/questionFour.java
@@ -1,13 +1,34 @@
-//package stringsMethods;
-//
-//public class questionFour {
-//    public static void main(String[] args){
-//        String[] name = "Hari Khanal";
-//
-//
-//    }
-//    public String[] four(String name){
-//
-//        return name.split;
-//    }
-//}
+package stringsMethods;
+
+import java.util.Scanner;
+
+public class questionFour {
+    public static void main(String[] args) {
+        Scanner src = new Scanner(System.in);
+        System.out.print("Enter your full name: ");
+        String a = src.nextLine();
+        String[] store =seperate(a);
+        for(int i =0; i<store.length;i++) {
+            if(i==0){
+            System.out.println(store[i]);
+            } else if (i==store.length-1) {
+                System.out.println(store[i]);
+            }
+
+
+        }
+    }
+    static String[] seperate(String name) {
+
+        String[] c = name.split(" ");
+//        String fullName=c[0];
+//        String lastName=c[1];
+        int d = c.length -1 ;
+//        System.out.println(c[0]);
+//        System.out.println(c[d]);
+        return c;
+
+    }
+
+
+}
